b3eaf8d5c96c056e99c0d9a677cfadfc1d391511
Raudo Moquete <sanchezz09@hotmail.com>
2020-04-17 20:54:32 -0400

    GraphQL schema extended, implementing resolver functions & adding authentication resolvers

---
+++ resolvers/Link.js
@@ -0,0 +1,7 @@
+function postedBy(root, args, context) {
+    return context.prisma.link({ id: root.id }).postedBy()
+}
+
+module.exports = {
+    postedBy,
+}
---
+++ resolvers/User.js
@@ -0,0 +1,7 @@
+function links(root, args, context) {
+    return context.prisma.user({ id: root.id }).links()
+}
+
+module.exports = {
+    links,
+}
