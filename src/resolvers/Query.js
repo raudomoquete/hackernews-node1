888ec3d70e66e02abe61663b09b248a59eab3185
Raudo Moquete <sanchezz09@hotmail.com>
2020-04-23 22:34:30 -0400

    adding filtering capabilities to the feed query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,6 +1,16 @@
 
- function feed(root, args, context, info) {
-    return context.prisma.links()
+ async function feed(root, args, context, info) {
+     const where = args.filter ? {
+         OR: [
+             { description_contains: args.filter },
+             { url_contains: args.filter },
+         ],
+     } : {}
+
+     const links = await context.prisma.links({
+         where
+     })
+    return links
 }
 
 module.exports = {
