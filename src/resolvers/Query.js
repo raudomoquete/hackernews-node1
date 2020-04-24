b91109a66179e1088ddefafc5c9044eaeeab4162
Raudo Moquete <sanchezz09@hotmail.com>
2020-04-24 17:01:12 -0400

    sorting aggregated & the returning the total amount of link elements function

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -10,9 +10,18 @@
      const links = await context.prisma.links({
          where,
          skip: args.skip,
-         first: args.first
+         first: args.first,
+         orderBy: args.orderBy
      })
-    return links
+     const count = await context.prisma.linksConnection({
+         where,
+     })
+     .aggregate()
+     .count()
+    return { 
+        links,
+        count,
+     } 
 }
 
 module.exports = {
